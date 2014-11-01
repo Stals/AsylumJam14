44a298bbc28e0da1a49f02b5ccac457227cdc387
Stanislav Korotaev <superstals@gmail.com>
2014-11-01 17:23:24 +0300

    game manager to get player and brother

---
+++ Asylum/Assets/Scripts/Game.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Game  {
+	private static Game instance;
+	private Game() {
+	}
+	public static Game Instance
+	{
+		get 
+		{
+			if (instance == null)
+			{
+				instance = new Game();
+			}
+			return instance;
+		}
+	}
+
+	GameManager manager;
+
+
+	public void init(GameManager _manager)
+	{
+		manager = _manager;
+	}
+
+	public GameManager getManager()
+	{
+		return manager;
+	}
+}
---
+++ Asylum/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum LevelState{
+	Running,
+	Ended
+}
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    GameObject player;
+	[SerializeField]
+    GameObject brother;
+
+	//[SerializeField]
+	//GameOverController gameOverController;
+    
+	//CameraShake cameraShake;
+   
+
+	// Use this for initialization
+	void Start () {
+		Game.Instance.init (this);
+		//cameraShake = Camera.main.gameObject.AddComponent("CameraShake") as CameraShake;
+		//currentLevelLabel.text = "Level: " + (Game.Instance.getCurrentLevelID () + 1).ToString();
+		//currentLevelState = LevelState.Running;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // TODO if button pressed -change world
+
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    CreateDeletingLine();
+        //}
+	}
+}
