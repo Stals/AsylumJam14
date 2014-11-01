c208d69019b8c70d39f18e9e4ffd547e606eb96c
Stanislav Korotaev <superstals@gmail.com>
2014-11-01 17:49:54 +0300

    added deactivation

--- Asylum/Assets/Scripts/Activatable/Activatable.cs
+++ Asylum/Assets/Scripts/Activatable/Activatable.cs
@@ -3,14 +3,21 @@ using System.Collections;
 
 public class Activatable : MonoBehaviour {
 
+    bool isTriggered = false;
+
 	// Use this for initialization
 	void Start () {
 	    // TODO add image to center
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+        if (isTriggered)
+        {
+            if (Input.GetKey (KeyCode.Space)) {
+                activate();
+            }
+        }
 	}
 
     /*
@@ -18,5 +25,13 @@ public class Activatable : MonoBehaviour {
      */
     protected virtual void activate(){
         // overriden method will be called
+    }
+
+    void OnTriggerEnter2D(Collider2D coll) {
+        isTriggered = true;
+    }
+    
+    void OnTriggerExit2D(Collider2D coll) {
+        isTriggered = false;
     }
 }
