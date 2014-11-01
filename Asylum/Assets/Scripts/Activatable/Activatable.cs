9e38d77d6a45effb790cd7922dcc6b9fa58ed37f
Stanislav Korotaev <superstals@gmail.com>
2014-11-01 17:42:16 +0300

    prefab update / scenes

---
+++ Asylum/Assets/Scripts/Activatable/Activatable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Activatable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    // TODO add image to center
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /*
+        ifistrigger - run overloaded activate
+     */
+    protected virtual void activate(){
+        // overriden method will be called
+    }
+}
---
+++ Asylum/Assets/Scripts/Activatable/ObjectDisabler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectDisabler : Activatable {
+
+    [SerializeField]
+    GameObject objectToDisable;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    override protected void activate(){
+        if (objectToDisable)
+        {
+            objectToDisable.SetActive(false);
+        }
+    }
+}
