349b30cd37abe6195d882d53bcb97047e253f0d3
Stanislav Korotaev <superstals@gmail.com>
2014-11-02 12:42:45 +0300

    monster enabler

---
+++ Asylum/Assets/Scripts/Activatable/Tile2MonterEnabler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile2MonterEnabler : Activatable {
+
+    [SerializeField]
+    GameObject enemy1;
+
+    [SerializeField]
+    GameObject enemy2;
+
+	// Use this for initialization
+	void Start () {
+        init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    override protected void activate(){
+        enemy1.SetActive(true);
+        enemy2.SetActive(true);
+    }
+}
