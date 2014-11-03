46c1913b719213a5fdcc5c28d8935b5b44511603
Stanislav Korotaev <superstals@gmail.com>
2014-11-03 11:44:54 +0300

    fixed the bridge

--- Asylum/Assets/Scripts/Tiles/Tile10Controller.cs
+++ Asylum/Assets/Scripts/Tiles/Tile10Controller.cs
@@ -5,6 +5,9 @@ public class Tile10Controller : TileContainer {
 
     bool showedNormalDialog = false;
 
+    [SerializeField]
+    FatherBehavior father;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -17,6 +20,37 @@ public class Tile10Controller : TileContainer {
 
     public override void tileEntered()
     {
+        GameObject player = Game.Instance.getManager().player;
+        GameObject brother = Game.Instance.getManager().brother;
+
+        if (Game.Instance.BadFather == false)
+        {
+            StartCoroutine(startCutscene(1f));
+            sayText("my children", 2f, father.gameObject);
+            sayText("father", 1f, brother);
+            //мелкий к папе
+            //???
+
+            //папа начинает идти на нас
+            // TODO after Delay
+            father.chasingDaughter = true;
+
+            player.GetComponent<WalkingPlayerController>().lookingAtFather = true;
+
+            //стаим флагчто папа стал плохой
+            Game.Instance.BadFather = true;
+
+            StartCoroutine(endCutscene(0f));
+        }
+
+        if (Game.Instance.FatherKilled)
+        {
+            StartCoroutine(startCutscene(1f));
+            sayText("where is father", 2f, brother);
+            sayText("he will catch up", 1f, player);
+        }
+
+
         /*
 cutscene
 если отец не убит
@@ -28,9 +62,10 @@ cutscene
 ставится флаг что отца убили 
 if(papa dead)
 по вощвращению новый бла бла
-            
          */
     }
+
+
     
     public override void darkWorldEntered()
     {
