42c31a4b58a07b80443a160417ed39b7277886ec
Stanislav Korotaev <superstals@gmail.com>
2014-11-03 13:29:53 +0300

    children dialogs

---
+++ Asylum/Assets/DialogTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogTrigger : Activatable {
+
+    [SerializeField]
+    GameObject speaker;
+
+    [SerializeField]
+    string text;
+
+	// Use this for initialization
+	void Start () {
+        init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    override protected void activate(){
+        GameObject player = Game.Instance.getManager().player;
+        GameObject brother = Game.Instance.getManager().brother;
+        TileContainer tile = player.GetComponent<WalkingPlayerController>().getCurrentTile();
+
+        tile.sayText(text, 2f, speaker);
+    }
+}
